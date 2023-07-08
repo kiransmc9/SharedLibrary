64e91205da9507707a0ca0d2d7efbf5e7746600d
kiransmc9 <137414869+kiransmc9@users.noreply.github.com>
2023-07-08 17:04:56 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,8 @@ def newDep(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+
+def newTest(test)
+{
+  git "https://github.com/intelliqittrainings/${test}.git"
+}
